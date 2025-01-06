5c682c9979acb65a8cbdbc0eb2864c8690a60393
SMirza3228 <mirzaseyidov38@gmail.com>
2025-01-06 18:40:57 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function show() {
+    const show = document.querySelector('.sidebar__square');
+    show.style.display = 'flex';
+}
+
+function close() {
+    const close = document.querySelector('.square');
+    close.style.display = 'none';
+}
